a10b6271b9047b02da4d992e952851944c5c2cbf
Jaemin.Park <once29@naver.com>
2023-11-06 15:15:19 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/mongoboard/MongoBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mongoboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
